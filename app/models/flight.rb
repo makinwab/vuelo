3f5626c945b2c4fb63f25e2e7efcc05b4dc6225a
andela-bmakinwa <makinwa37@gmail.com>
2016-01-11 00:17:15 +0100

    Seed flight model and add rubocop file

--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,6 +1,6 @@
 class Flight < ActiveRecord::Base
-  belongs_to :from_airport, class: "Airport", foreign_key: "from_airport_id"
-  belongs_to :to_airport, class: "Airport", foreign_key: "to_airport_id"
+  belongs_to :from_airport, class_name: "Airport", foreign_key: "from_airport_id"
+  belongs_to :to_airport, class_name: "Airport", foreign_key: "to_airport_id"
 
   def self.get_trip(airport_id)
     where("from_airport_id = :airport OR to_airport_id = :airport", airport: airport_id)
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,5 @@
 require "csv"
+require "faker"
 
 Airport.destroy_all
 Flight.destroy_all
@@ -12,5 +13,17 @@ airlines = ["Arik Air", "Dana Air", "Aero Contractors", "Air Peace",
 "Allied Air", "Associated Aviation", "Azman Air", "Dornier Aviation Nigeria",
 "Kabo Air", "Max Air", "Med-View Airline", "Overland Airway"]
 
+50.times do |i|
+  from_airport_id = Airport.ids.sample
+  f = Flight.new
+  f.from_airport_id = from_airport_id
+  f.to_airport_id = Airport.where.not(id: from_airport_id).ids.sample
+  f.departure_date = Faker::Time.forward(23, :morning)
+  f.arrival_date = f.departure_date + 45.minutes
+  f.airline = airlines.sample
+  f.price = Faker::Commerce.price
+
+  f.save!
+end
 
 puts "Seeding completed"
